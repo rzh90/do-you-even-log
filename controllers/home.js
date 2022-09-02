2dfeb14d319e076f915c6cfbfd260952b0779c84
ruinaz90 <rzhao9002@gmail.com>
2022-09-02 16:35:51 +0000

    added sign up and login

--- controllers/home.js
+++ controllers/home.js
@@ -1,5 +1,8 @@
 module.exports = {
     getIndex: (req,res)=>{
         res.render('index.ejs')
+    },
+    getDashboard: (req,res)=>{
+        res.render('dashboard.ejs')
     }
 }
--- routes/main.js
+++ routes/main.js
@@ -6,4 +6,12 @@ const { ensureAuth, ensureGuest } = require('../middleware/auth')
 
 router.get('/', homeController.getIndex)
 
+router.get('/login', authController.getLogin)
+router.post('/login', authController.postLogin)
+
+router.get('/signup', authController.getSignup)
+router.post('/signup', authController.postSignup)
+
+router.get('/dashboard', ensureAuth, homeController.getDashboard)
+
 module.exports = router
