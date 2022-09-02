55a3df2ee4e2d86fe1813daf842ed42dabf75d8a
ruinaz90 <rzhao9002@gmail.com>
2022-09-02 19:03:26 +0000

    installed passport 0.5

--- routes/main.js
+++ routes/main.js
@@ -9,6 +9,8 @@ router.get('/', homeController.getIndex)
 router.get('/login', authController.getLogin)
 router.post('/login', authController.postLogin)
 
+router.get('/logout', authController.logout)
+
 router.get('/signup', authController.getSignup)
 router.post('/signup', authController.postSignup)
 
