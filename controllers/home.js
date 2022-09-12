f629a63d4a3ea0da898987eeb5165f094c92ebe7
ruinaz90 <rzhao9002@gmail.com>
2022-09-12 15:19:55 +0000

    added dashboard items

--- controllers/home.js
+++ controllers/home.js
@@ -1,16 +1,22 @@
 const Exercise = require("../models/Exercise")
 
 module.exports = {
-    getIndex: (req,res) => {
+    getIndex: (req, res) => {
         res.render('index.ejs')
     },
     
-    getDashboard: async(req,res) => {
+    getDashboard: async(req, res) => {
         try {
-            const numOfWorkouts = await Exercise.countDocuments({userId:req.user.id}) //find number of exercises entered by user
-            const workoutItems = await Exercise.find({userId: req.user.id}).sort({date: "desc"}).lean() //find all exercises entered by user
-
-            res.render('dashboard.ejs', {user: req.user, workouts: workoutItems, workoutCount: numOfWorkouts})
+            const numOfExercises = await Exercise.countDocuments({userId:req.user.id}) //find number of exercises entered by user
+            const exerciseItems = await Exercise.find({userId: req.user.id}).sort({date: "desc"}).lean() //find all exercises entered by user
+            const exerciseByDate = exerciseItems.reduce(function(item, doc) { //group array of objects (exercises) by key (date)
+                item[doc.date] = item[doc.date] || []
+                item[doc.date].push(doc)
+                return item
+            }, Object.create(null))
+            
+            //render page with user and exercise items
+            res.render('dashboard.ejs', {user: req.user, exercises: exerciseItems, exerciseCount: numOfExercises, exerciseDates: exerciseByDate})
         }
         catch(err) {
             console.error(err)
