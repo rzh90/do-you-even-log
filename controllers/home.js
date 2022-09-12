c5d7fa743718fa870ee26a606909040c0d340da0
ruinaz90 <rzhao9002@gmail.com>
2022-09-12 14:01:05 +0000

    fixed styling and added dashboard stats

--- controllers/home.js
+++ controllers/home.js
@@ -1,9 +1,19 @@
+const Exercise = require("../models/Exercise")
+
 module.exports = {
-    getIndex: (req,res)=>{
+    getIndex: (req,res) => {
         res.render('index.ejs')
     },
     
-    getDashboard: (req,res)=>{
-        res.render('dashboard.ejs', {user: req.user})
+    getDashboard: async(req,res) => {
+        try {
+            const numOfWorkouts = await Exercise.countDocuments({userId:req.user.id}) //find number of exercises entered by user
+            const workoutItems = await Exercise.find({userId: req.user.id}).sort({date: "desc"}).lean() //find all exercises entered by user
+
+            res.render('dashboard.ejs', {user: req.user, workouts: workoutItems, workoutCount: numOfWorkouts})
+        }
+        catch(err) {
+            console.error(err)
+        }
     }
 }
